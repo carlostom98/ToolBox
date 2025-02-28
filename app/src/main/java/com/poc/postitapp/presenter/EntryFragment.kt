5edfae9e5c82860802ae57028d6727cdde5ae751
Carlos Andrés Martínez Suárez <carlos.martinez05@globant.com>
2025-02-28 16:12:14 -0500

    feature: compose screens creation, room modification

--- app/src/main/java/com/poc/postitapp/presenter/EntryFragment.kt
+++ app/src/main/java/com/poc/postitapp/presenter/EntryFragment.kt
@@ -5,24 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.poc.persistence.domain.entities.PostItEntity
+import com.poc.persistence.domain.entities.UrgencyLevel
 import com.poc.postitapp.R
 import com.poc.postitapp.databinding.FragmentEntryBinding
+import com.poc.postitapp.presenter.viewintents.crudintent.CRUDIntents
 
 class EntryFragment : Fragment() {
 
     private var _binding: FragmentEntryBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var activity: MainActivity
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        activity = getActivity() as MainActivity
         _binding = FragmentEntryBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -30,8 +46,30 @@ class EntryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         with(binding) {
             composeView.setContent {
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    Text("Hello World!!")
+
+                var textFieldOne by remember {
+                    mutableStateOf("")
+                }
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Row(modifier = Modifier.wrapContentHeight()) {
+                        Button(onClick = { activity.manageDataViewModel.handleIntent(
+                            CRUDIntents.UpsertPostIt(
+                                PostItEntity(
+                                    0,
+                                    title = textFieldOne,
+                                    urgencyLevel = UrgencyLevel.HIGH,
+                                    color = "#FFFFFF",
+                                    description = "Hello this is my first PostIt"
+                                )
+                            )
+                        )}) {
+                            Text(text = "Click me")
+                        }
+
+                        TextField(value = textFieldOne, onValueChange = {
+                            textFieldOne = it
+                        })
+                    }
                 }
             }
         }
--- persistence/src/main/java/com/poc/persistence/domain/entities/PostItEntity.kt
+++ persistence/src/main/java/com/poc/persistence/domain/entities/PostItEntity.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "PostIt")
 data class PostItEntity (
-   @PrimaryKey(autoGenerate = true) val id: Int ?,
+   @PrimaryKey(autoGenerate = true) val id: Int = 0,
    @ColumnInfo (name = "title") val title: String ?,
    @ColumnInfo (name = "description") val description: String ?,
    @ColumnInfo (name = "color") val color: String ?,
