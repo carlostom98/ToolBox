5011daec11368bef18a19ffd88dbfee4c91bc02a
Carlos Andrés Martínez Suárez <carlos.martinez05@globant.com>
2025-02-28 15:44:35 -0500

    feature: compose screens creation

--- app/src/main/java/com/poc/postitapp/presenter/MainActivity.kt
+++ app/src/main/java/com/poc/postitapp/presenter/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.poc.postitapp.presenter
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.poc.postitapp.databinding.ActivityMainBinding
+import com.poc.postitapp.presenter.viewintents.crudintent.ManageDataViewModel
 import com.poc.postitapp.utils.extensions.changeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    val manageDataViewModel: ManageDataViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
