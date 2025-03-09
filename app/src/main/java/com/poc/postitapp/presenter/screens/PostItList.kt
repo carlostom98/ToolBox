4651bd095f503787e2fcac45a207cb623e882edc
Carlos Andrés Martínez Suárez <carlos.martinez05@globant.com>
2025-03-09 12:41:38 -0500

    fix: swipe for delete fixed

--- app/src/main/java/com/poc/postitapp/presenter/screens/PostItList.kt
+++ app/src/main/java/com/poc/postitapp/presenter/screens/PostItList.kt
@@ -89,7 +89,7 @@ fun FloatButton(modifier: Modifier, onClick: () -> Unit) {
 @Composable
 fun SwipeToDeleteContainer(
     onDelete: () -> Unit,
-    animationDuration: Int = 500,
+    animationDuration: Int = 1000,
     content: @Composable () -> Unit
 ) {
 
@@ -119,13 +119,14 @@ fun SwipeToDeleteContainer(
             DeleteBackGround(swipeDismissState = state)
         }, content = {
             content()
-        })
+        }, enableDismissFromStartToEnd = false)
     }
     
     LaunchedEffect(key1 = isRemoved) {
         if (isRemoved) {
             delay(animationDuration.toLong())
             onDelete()
+            isRemoved = false
         }
     }
 
@@ -149,6 +150,7 @@ fun DeleteBackGround(swipeDismissState: SwipeToDismissBoxState) {
     ) {
 
         Icon(
+            modifier = Modifier.size(32.dp),
             imageVector = Icons.Default.Delete,
             contentDescription = "Delete item",
             tint = Color.Black
